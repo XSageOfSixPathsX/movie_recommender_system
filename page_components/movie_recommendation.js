16c9d100bbc93d8de38c975de527b18c88875c38
Prakhar Jain <prakhar.jain@geeksforgeeks.org>
2023-10-07 01:43:50 +0530

    minor css changes

--- page_components/movie_recommendation.js
+++ page_components/movie_recommendation.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react'
-import axios from 'axios'
 import "../styles/movieRecommendationStyles.scss"
 
 async function fetchMovieData(movieName) {
@@ -61,7 +60,7 @@ function MovieRecommendation(props) {
             </div>
           </div>
         ) : (
-          <p>Loading...</p>
+          <div className='load'>Loading....</div>
         )}
     </div>
   );
